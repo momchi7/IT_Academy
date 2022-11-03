23c9a093da94688b7bfde4ed77dcd0418cf6cda0
momchi7 <81459198+momchi7@users.noreply.github.com>
2022-11-03 21:49:18 +0100

    Update Exercici_4.sql

--- Sprint_1/Exercici_4/Exercici_4.sql
+++ Sprint_1/Exercici_4/Exercici_4.sql
@@ -1,1 +1,8 @@
 
+SELECT person_details.person_name,
+	     COUNT(movie_person.role_id) AS roles_per_movie 
+       FROM movies.tb_person person_details
+INNER JOIN movies.tb_movie_person movie_person
+ON person_details.person_id = movie_person.person_id
+GROUP BY person_name
+HAVING roles_per_movie>1;
