36abcc17c946cda3351fff4d2312b991f990c970
momchi7 <81459198+momchi7@users.noreply.github.com>
2022-11-15 13:33:53 +0100

    Create Ejercicio_2.py

---
+++ Sprint_2/Tasca_M2_T01/Ejercicio_2.py
@@ -0,0 +1,14 @@
+trimestres = [["enero", "febrero", "marzo"],
+              ["abril", "mayo", "junio"],
+              ["julio", "agosto", "septiembre"],
+              ["octubre", "noviembre", "diciembre"]]
+
+#El segon mes del primer trimestre.
+trimestres[0][1]
+
+#Els mesos del primer trimestre.
+trimestres[0]
+
+#Setembre i octubre.
+trimestres[2][2]
+trimestres[3][0]
